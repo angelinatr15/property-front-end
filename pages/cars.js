4ff9df82090073a392e6182cf0b465d5ff15fe3b
Angelina Truong <angelinatruong@Angelinas-MacBook-Air.local>
2022-02-08 19:03:28 -0800

    first commit

---
+++ pages/cars.js
@@ -0,0 +1,42 @@
+import React, { useRef, useState } from 'react'
+
+const cars = () => {
+  const userRef = useRef()
+  const [cars, setCars] = useState([])
+
+  async function changeUser() {
+    console.log(userRef.current.value)
+    const name = userRef.current.value
+
+    const url = 'http://glacial-journey-23518.herokuapp.com/cars/' + name
+
+    try {
+      const response = await fetch(url)
+      const cars = await response.json()
+      console.log(cars)
+      setCars(cars)
+    } catch (e) {
+      console.log(e.message)
+    }
+  }
+
+  return (
+    <div>
+      <select ref={userRef} onChange={changeUser}>
+        <option value="Ryan Nguyen">Ryan Nguyen</option>
+        <option value="Alvin Nguyen">Alvin Nguyen</option>
+        <option value="Angelina Truong">Angelina Truong</option>
+      </select>
+
+      {cars.map((car) => (
+        <div key={car.id}>
+          {car.year}
+          {car.make}
+          {car.model}
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default cars
